37ee49296de4d91bc2d3c94f6f5847013b3ac662
eduardo <dowen_azteca81@hotmail.com>
2020-04-26 20:34:57 -0500

    Ahora el index fue modificado

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-<?php
-echo "Hola mundo";
-?>
+Aprende a Dominar una de las herramienta más utilizadas por todos los desarrolladores web, programadores y expertos en código profesionales, llamada Git, un sistema de Control de versiones open source, creado por Linus Torvalds.
+Git permite el manejo de código a lo largo del tiempo, permitiendo la colaboración en un equipo de programadores, desde cualquier parte del mundo. 
+En este curso Aprenderemos desde como utilizar los comandos Básicos del bash de Git, hasta como subir nuestro Código a Github.
